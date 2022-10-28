a8d1f20669e701f9e611941a5bbf99e272eee519
Maarten A. Breddels <maartenbreddels@gmail.com>
2022-10-28 14:44:10 +0200

    add custom javascript

---
+++ covid/assets/custom.js
@@ -0,0 +1,35 @@
+var elem = document.documentElement;
+var state = 0;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+function fullscreen(){
+    if (state == 0){
+        openFullscreen();
+        state = 1;
+    } else if (state == 1){
+        closeFullscreen();
+        state = 0;
+    }
+}
